c074954004c755d4875ee1e85755b208346e8c08
Mariano Spadaccini <spadacciniweb@gmail.com>
2026-07-14 11:33:29 +0200

    Some change to waffle bar 2 square

--- embed.js
+++ embed.js
@@ -1,24 +1,24 @@
 (() => {
-    const WORKSPACE = new URL(document.currentScript.src).origin;
-
+    const WORKSPACE =
+        new URL(document.currentScript.src).origin;
     if (document.getElementById("wafflebar")) {
         return;
     }
-
     function create() {
-
         const iframe = document.createElement("iframe");
         iframe.id = "wafflebar";
         iframe.src =
             WORKSPACE +
             "/?parent=" +
-            encodeURIComponent(window.location.origin);
+            encodeURIComponent(
+                window.location.origin
+            );
         Object.assign(iframe.style, {
             position: "fixed",
             top: "0",
             left: "0",
-            width: "100%",
-            height: "44px",
+            width: "52px",
+            height: "52px",
             border: "0",
             margin: "0",
             padding: "0",
@@ -26,24 +26,43 @@
             overflow: "hidden",
             zIndex: "2147483647"
         });
-
-        iframe.setAttribute("scrolling", "no");
-        document.body.appendChild(iframe);
-        window.addEventListener("message", function (event) {
-            if (event.origin !== WORKSPACE)
-                return;
-            switch (event.data.type) {
-                case "wafflebar-open":
-                    iframe.style.height = "100vh";
-                    break;
-                case "wafflebar-close":
-                    iframe.style.height = "44px";
-                    break;
+        iframe.setAttribute(
+            "scrolling",
+            "no"
+        );
+        document.body.appendChild(
+            iframe
+        );
+        window.addEventListener(
+            "message",
+            function (event) {
+                if (event.origin !== WORKSPACE) {
+                    return;
+                }
+                if (
+                    !event.data ||
+                    event.data.source !== "wafflebar"
+                ) {
+                    return;
+                }
+                switch (event.data.type) {
+                    case "wafflebar-open":
+                        iframe.style.width = "100%";
+                        iframe.style.height = "100vh";
+                        break;
+                    case "wafflebar-close":
+                        iframe.style.width = "52px";
+                        iframe.style.height = "52px";
+                        break;
+                }
             }
-        });
+        );
     }
     if (document.readyState === "loading") {
-        document.addEventListener("DOMContentLoaded", create);
+        document.addEventListener(
+            "DOMContentLoaded",
+            create
+        );
     } else {
         create();
     }
